d575a1b337e5b373a92ab16f33a672a8b253d7a6
KlausHasa <klaushasa18@gmail.com>
2024-01-16 18:05:12 +0100

    f

--- server/server.js
+++ server/server.js
@@ -7,9 +7,19 @@ app.use(express.json());                           /* This is new and allows JSO
 app.use(express.urlencoded({ extended: true }));   /* This is new and allows JSON Objects with strings and arrays*/
 require('./config/mongoose.config');  
   /* This is new */
-require('./routes/pirates.routes')(app);
-
-
+  const PiratesController = require('./controllers/pirates.controller');
+  module.exports = (app) => {
+      app.get('/pirates', PiratesController.findAll);
+      app.get('/pirate/:id', PiratesController.findOne);
+      app.get('/filtered-pirates/:crew', PiratesController.findFilteredPirates);
+      app.post('/pirate', PiratesController.createNew);
+      app.put('/pirate/edit/:id', PiratesController.update);
+      app.delete('/pirate/:id', PiratesController.delete);
+  }
+  
+  
+  
+  
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
 })
